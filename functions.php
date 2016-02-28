5fcd2807d698a3012f68971801c9ea5edb67aeff
Ruben Langius <langiusruben@gmail.com>
2016-02-28 18:38:47 +0100

    layout modifications
    layout modifications

--- functions.php
+++ functions.php
@@ -40,4 +40,42 @@ function childtheme_customize_register( $wp_customize )
 }
 add_action( 'customize_register', 'childtheme_customize_register' );
 
+// change the excerpt function from the parent theme
+
+function twentysixteen_excerpt( $class = 'excerpt' ) {
+    $class = esc_attr( $class );
+
+    if ( has_excerpt() || is_search() ) : ?>
+        <div class="<?php echo $class; ?>">
+            <?php the_excerpt(); ?>
+        </div><!-- .<?php echo $class; ?> -->
+    <?php endif;
+}
+
+// change the thumbnail function
+
+function twentysixteen_post_thumbnail() {
+    if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+        return;
+    }
+
+    if ( is_singular() ) :
+    ?>
+
+    <div class="post-thumbnail">
+        <?php the_post_thumbnail(); ?>
+    </div><!-- .post-thumbnail -->
+
+    <?php else : ?>
+
+    <a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
+        <?php the_post_thumbnail( 'post-thumbnail', array( 'alt' => the_title_attribute( 'echo=0' ) ) ); ?>
+    </a>
+
+    <?php endif; // End is_singular()
+}
+
+add_image_size( 'loop-thumb', 600, 600 ); 
+
+
 ?>
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,27 +10,34 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
-		<div class="container featured">
-			<div class="col-sm-6 feature-image">
-				<?php twentysixteen_post_thumbnail(); ?>
-			</div>
-			<div class="col-sm-6 sidetext">
-				<span class="sticky-post"><?php _e( 'Featured', 'twentysixteen' ); ?></span>
-				<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-				<?php twentysixteen_excerpt(); ?>
+		<div class="container-fluid featured">
+			<div class="row">
+				<div class="col-sm-6 hidden-xs feature-image">
+					<?php twentysixteen_post_thumbnail(); ?>
+				</div>
+				<div class="col-sm-6 sidetext">
+					<span class="sticky-post"><?php _e( 'Featured', 'twentysixteen' ); ?></span>
+					<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+					<?php twentysixteen_excerpt(); ?>
+				</div>
 			</div>
 		</div>
 	<?php else: ?>
 
-
-	<header class="col-sm-12">		
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
-	<div class="col-sm-5">
-		<?php twentysixteen_post_thumbnail(); ?>
-	</div>
-	<div class="col-sm-6">
-		<?php twentysixteen_excerpt(); ?>
+	<div class="container-fluid">
+		<div class="row">
+			<header class="col-sm-12">		
+				
+			</header><!-- .entry-header -->
+			<div class="col-sm-5 hidden-xs">
+				<?php twentysixteen_post_thumbnail(); ?>
+			</div>
+			<div class="col-sm-6">
+				<?php twentysixteen_entry_taxonomies() ?>
+				<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+				<?php twentysixteen_excerpt() ?>
+			</div>
+		</div>
 	</div>
 
 
